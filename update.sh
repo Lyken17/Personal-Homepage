8ca842544e1d2c9c2012d2d049255effa8f6d4a9
Ligeng Zhu <lykensyu+gitlab@gmail.com>
2018-12-11 15:24:14 -0500

    updated in 15:24-11/Dec/2018

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 hugo -s .
 rm -rf docs/post/
 rm -rf docs/publication/
-rm -rf docs/project/
+# rm -rf docs/project/
 git add -A
 git commit -m "updated in $(date +%H:%M-%d/%b/%Y)"
 git push -u origin master
