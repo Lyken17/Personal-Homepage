e1dae147c8a39bac7dbbc09f88bca770ead10f29
Ligeng Zhu <ligeng@mit.edu>
2020-04-17 18:37:47 -0400

    updated in 18:37-17/Apr/2020

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
-hugo -s .
-rm -rf docs/post/
-rm -rf docs/publication/
+# hugo -s .
+# rm -rf docs/post/
+# rm -rf docs/publication/
 # rm -rf docs/project/
 git add -A
 git commit -m "updated in $(date +%H:%M-%d/%b/%Y)"
